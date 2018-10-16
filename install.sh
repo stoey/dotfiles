d7c0f58c688138c00696c4f278a138c88ff17994
Dan Head <dan@singleops.com>
2018-10-16 16:12:20 -0500

    Cleaned up install script

--- install.sh
+++ install.sh
@@ -1,19 +1,26 @@
 #/bin/sh -e
 
+# Functions
+
+FILE_EXISTS () { [ -f $1 ] }
+DIR_EXISTS () { [ -d $1 ] }
+FILE_CONTAINS () { fgrep -q $2 $1 }
+
 # Setup Vim
 
 ## Install pathogen
-[ -d "${HOME}/.vim/autoload" ] || mkdir -p "${HOME}/.vim/autoload"
-[ -d "${HOME}/.vim/bundle" ] || mkdir -p "${HOME}/.vim/bundle"
-[ -f "${HOME}/.vim/autoload/pathogen.vim" ] || curl -LSso "${HOME}/.vim/autoload/pathogen.vim" "https://tpo.pe/pathogen.vim"
-fgrep -q "pathogen#infect()" "${HOME}/.vimrc" || echo "
+
+DIR_EXISTS "${HOME}/.vim/autoload" || mkdir -p "${HOME}/.vim/autoload"
+DIR_EXISTS "${HOME}/.vim/bundle" || mkdir -p "${HOME}/.vim/bundle"
+FILE_EXISTS "${HOME}/.vim/autoload/pathogen.vim" || curl -LSso "${HOME}/.vim/autoload/pathogen.vim" "https://tpo.pe/pathogen.vim"
+FILE_CONTAINS "${HOME}/.vimrc" "pathogen#infect()" || echo "
 execute pathogen#infect()
 syntax on
 filetype plugin indent on
 " >> "${$HOME}/.vimrc"
 
 ## Install CoffeeScript plugin
-[ -d "${HOME}/.vim/bundle/vim-coffee-script/" ] || git clone "https://github.com/kchmck/vim-coffee-script.git" "${HOME}/.vim/bundle/vim-coffee-script/"
+DIR_EXISTS "${HOME}/.vim/bundle/vim-coffee-script/" || git clone "https://github.com/kchmck/vim-coffee-script.git" "${HOME}/.vim/bundle/vim-coffee-script/"
 (cd ${HOME}/.vim/bundle/vim-coffee-script/ && git pull)
 
 
